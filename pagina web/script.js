613865ec8314f1c698510306008cb85108b88081
wkl0ree <162451285+wkl0ree@users.noreply.github.com>
2025-05-28 23:23:59 -0400

    Add files via upload

---
+++ pagina web/script.js
@@ -0,0 +1,201 @@
+const menuBtn = document.getElementById("menu-btn");
+const navLinks = document.getElementById("nav-links");
+const menuBtnIcon = menuBtn.querySelector("i");
+
+menuBtn.addEventListener("click", (e) => {
+  navLinks.classList.toggle("open");
+
+  const isOpen = navLinks.classList.contains("open");
+  menuBtnIcon.setAttribute(
+    "class",
+    isOpen ? "ri-close-line" : "ri-menu-3-line"
+  );
+});
+
+navLinks.addEventListener("click", (e) => {
+  navLinks.classList.remove("open");
+  menuBtnIcon.setAttribute("class", "ri-menu-3-line");
+});
+
+const scrollRevealOption = {
+  distance: "50px",
+  origin: "bottom",
+  duration: 1000,
+};
+
+ScrollReveal().reveal(".header__image img", {
+  duration: 1000,
+});
+ScrollReveal().reveal(".header__content h1", {
+  ...scrollRevealOption,
+  delay: 500,
+});
+ScrollReveal().reveal(".header__content .section__description", {
+  ...scrollRevealOption,
+  delay: 1000,
+});
+ScrollReveal().reveal(".header__btn", {
+  ...scrollRevealOption,
+  delay: 1500,
+});
+ScrollReveal().reveal(".header__content .socials", {
+  ...scrollRevealOption,
+  delay: 2000,
+});
+
+ScrollReveal().reveal(".popular__card", {
+  ...scrollRevealOption,
+  interval: 500,
+});
+
+ScrollReveal().reveal(".discover__card img", {
+  ...scrollRevealOption,
+  origin: "left",
+});
+ScrollReveal().reveal(".discover__card:nth-child(2) img", {
+  ...scrollRevealOption,
+  origin: "right",
+});
+ScrollReveal().reveal(".discover__card__content h4", {
+  ...scrollRevealOption,
+  delay: 500,
+});
+ScrollReveal().reveal(".discover__card__content .section__description", {
+  ...scrollRevealOption,
+  delay: 1000,
+});
+ScrollReveal().reveal(".discover__card__content h3", {
+  ...scrollRevealOption,
+  delay: 1500,
+});
+ScrollReveal().reveal(".discover__card__btn", {
+  ...scrollRevealOption,
+  delay: 2000,
+});
+
+ScrollReveal().reveal(".banner__content .section__header", {
+  ...scrollRevealOption,
+});
+ScrollReveal().reveal(".banner__content .section__description", {
+  ...scrollRevealOption,
+  delay: 500,
+});
+ScrollReveal().reveal(".banner__card", {
+  ...scrollRevealOption,
+  delay: 1000,
+  interval: 500,
+});
+
+ScrollReveal().reveal(".subscribe__content .section__header", {
+  ...scrollRevealOption,
+});
+ScrollReveal().reveal(".subscribe__content .section__description", {
+  ...scrollRevealOption,
+  delay: 500,
+});
+ScrollReveal().reveal(".subscribe__content form", {
+  ...scrollRevealOption,
+  delay: 1000,
+});
+
+
+ScrollReveal().reveal(".header__content h4", {
+  ...scrollRevealOption,
+});
+ScrollReveal().reveal(".header__content h1", {
+  ...scrollRevealOption,
+  delay: 500,
+});
+ScrollReveal().reveal(".header__content h2", {
+  ...scrollRevealOption,
+  delay: 1000,
+});
+ScrollReveal().reveal(".header__content p", {
+  ...scrollRevealOption,
+  delay: 1500,
+});
+ScrollReveal().reveal(".header__btn", {
+  ...scrollRevealOption,
+  delay: 2000,
+});
+
+
+
+
+
+
+
+
+
+
+
+
+ScrollReveal().reveal(".header__content h4", {
+  ...scrollRevealOption,
+});
+ScrollReveal().reveal(".header__content h1", {
+  ...scrollRevealOption,
+  delay: 500,
+});
+ScrollReveal().reveal(".header__content h2", {
+  ...scrollRevealOption,
+  delay: 1000,
+});
+ScrollReveal().reveal(".header__content p", {
+  ...scrollRevealOption,
+  delay: 1500,
+});
+ScrollReveal().reveal(".header__btn", {
+  ...scrollRevealOption,
+  delay: 2000,
+});
+
+ScrollReveal().reveal(".intro__card", {
+  ...scrollRevealOption,
+  interval: 500,
+});
+
+ScrollReveal().reveal(
+  ".about__row:nth-child(3) .about__image img, .about__row:nth-child(5) .about__image img",
+  {
+    ...scrollRevealOption,
+    origin: "left",
+  }
+);
+ScrollReveal().reveal(".about__row:nth-child(4) .about__image img", {
+  ...scrollRevealOption,
+  origin: "right",
+});
+ScrollReveal().reveal(".about__content span", {
+  ...scrollRevealOption,
+  delay: 500,
+});
+ScrollReveal().reveal(".about__content h4", {
+  ...scrollRevealOption,
+  delay: 1000,
+});
+ScrollReveal().reveal(".about__content p", {
+  ...scrollRevealOption,
+  delay: 1500,
+});
+
+ScrollReveal().reveal(".product__card", {
+  ...scrollRevealOption,
+  interval: 500,
+});
+
+ScrollReveal().reveal(".service__card", {
+  duration: 1000,
+  interval: 500,
+});
+
+const swiper = new Swiper(".swiper", {
+  slidesPerView: 3,
+  spaceBetween: 20,
+  loop: true,
+});
+
+ScrollReveal().reveal(".instagram__grid img", {
+  duration: 1000,
+  interval: 500,
+});
